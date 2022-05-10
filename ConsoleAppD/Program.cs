71ea32834e712d4db2f671a1868d106334f3d3e9
andreyshmelev <andrshmel@gmail.com>
2022-05-10 08:27:28 +0300

    так ну тут вроде выдает тему

--- ConsoleAppD/Program.cs
+++ ConsoleAppD/Program.cs
@@ -1,90 +1,117 @@
 ﻿using System;
-
+#pragma warning disable CS8604
+#pragma warning disable CS8602
 public class Program
 {
     public static void Main(string[] args)
     {
 
-        using (StreamReader file = new StreamReader("C:\\Users\\Brux\\Downloads\\testD\\tests\\01"))
+        using (StreamReader file = new StreamReader("C:\\Users\\Brux\\Downloads\\testD\\tests\\04"))
         {
+            char[] alphabeth = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".ToCharArray();
+            int[] usedLetterFirst  = new int[alphabeth.Length];
+            int[] usedLetterSecond = new int[alphabeth.Length];
+
+            Array.Fill(usedLetterFirst, 0);
+            Array.Fill(usedLetterSecond, 0);
 
             var firstWord = file.ReadLine();
             var secondWord = file.ReadLine();
 
-            string resultword = new String('_', firstWord.Length); 
+            char[] aaa = firstWord.ToCharArray();
+            char[] bbb = secondWord.ToCharArray();
 
-            Console.WriteLine(firstWord);
-            Console.WriteLine(secondWord);
+
+            string resultword = new String('_', firstWord.Length);
+
+
 
             char[] resultCharArr = resultword.ToCharArray();
 
 
             for (int i = 0; i < firstWord.Length; i++)
             {
-                if (firstWord.ElementAt(i) == secondWord.ElementAt(i))
+                var letterFirstWord = aaa[i];
+                var letterSecondWord = bbb[i];
+
+                if (letterFirstWord == letterSecondWord) // если буква равна букве
                 {
-                    resultCharArr[i] = firstWord.ElementAt(i);
-                    Console.WriteLine("a");
-
-                }
-                else {
-
-                    Console.WriteLine("z");
-                    resultword.Append((char)'z');
+                    resultCharArr[i] = 'G';
+                    aaa[i] = ' ';
+                    bbb[i] = ' ';
                 }
             }
 
 
-            Console.WriteLine(String.Join("", resultCharArr));
+            for (int i = 0; i < firstWord.Length; i++)
+            {
+                var letterFirstWord = aaa[i];
+                var letterSecondWord = bbb[i];
 
-            /*for (var j = 0; j < 0; j++)
+                var indFirst  = Array.IndexOf(alphabeth, letterFirstWord);
+                var indSecond = Array.IndexOf(alphabeth, letterSecondWord);
+
+                if (indFirst >= 0)
+                {
+                    usedLetterFirst[indFirst]++;
+                }
+
+                if (indSecond >= 0)
+                {
+                    usedLetterSecond[indSecond]++;
+                }
+
+                if (letterFirstWord == letterSecondWord) // если буква равна букве
+                {
+                    resultCharArr[i] = 'G';
+                    //usedLetter[indd]--;
+                    aaa[i] = ' ';
+                    bbb[i] = ' ';
+                }
+            }
+
+            for (int i = 0; i < secondWord.Length; i++)
             {
 
-                var qqq = Console.ReadLine();
-                var count = int.Parse(qqq);
-
-                var input = Console.ReadLine();
-                if (!string.IsNullOrEmpty(input))
+                if (bbb[i] != ' ')
                 {
-                    var tasks = input.Split(" ");
-                    var collection = new int[tasks.Length];
-                    var indexes = new int[tasks.Length];
+                    var letterSecondWord = bbb[i];
+                    var indSecond = Array.IndexOf(alphabeth, letterSecondWord);
 
-                    for (var i = 0; i < tasks.Length; i++)
+                    if (indSecond >= 0)
                     {
-                        int.TryParse(tasks[i], out collection[i]);
-                        indexes[i] = i;
-                    }
-
-                    Array.Sort(collection, indexes);
-
-                    Array.Reverse(collection);
-                    Array.Reverse(indexes);
-
-                    var niceCollection = new int[collection.Length];
-                    var newCollection = new int[collection.Length];
-                    var maxvalue = collection[0];
-
-                    int nice = 1;
-
-                    for (int i = 0; i < collection.Length; i++)
-                    {
-                        if (collection[i] < maxvalue - 1)
+                        if (usedLetterFirst[indSecond] > 0)
                         {
-                            nice++;
-                            maxvalue = collection[i];
+                            resultCharArr[i] = 'Y';
+                            usedLetterFirst[indSecond]--;
                         }
-                        niceCollection[i] = nice;
+                        else
+                        {
+                            resultCharArr[i] = '.';
+                        }
                     }
 
-                    for (int i = 0; i < collection.Length; i++)
-                    {
-                        newCollection[indexes[i]] = niceCollection[i];
-
-                    }
-                    Console.WriteLine(String.Join(" ", newCollection));
                 }
-            }*/
-        }
+            }
+            
+
+
+
+            //Console.WriteLine(String.Join("", resultCharArr));
+
+           // Console.WriteLine(String.Join(" ", aaa));
+           // Console.WriteLine(String.Join(" ", bbb));
+
+           // Console.WriteLine(String.Join(" ", alphabeth));
+            //Console.WriteLine(String.Join(" ", usedLetterFirst));
+             //   Console.WriteLine(String.Join(" ", usedLetterSecond));
+                Console.WriteLine(String.Join("", resultCharArr));
+
+                //Console.WriteLine(String.Join("+", aaa));
+
+                //Console.WriteLine(String.Join("-", bbb));
+
+
+            }
     }
 }
