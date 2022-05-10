f04cea17bc3fcd47a7c16468a28422fee7499427
andreyshmelev <andrshmel@gmail.com>
2022-05-10 08:32:16 +0300

    после бьюти

--- ConsoleAppD/Program.cs
+++ ConsoleAppD/Program.cs
@@ -1,15 +1,14 @@
 ﻿using System;
-#pragma warning disable CS8604
-#pragma warning disable CS8602
+
 public class Program
 {
     public static void Main(string[] args)
     {
 
-        using (StreamReader file = new StreamReader("C:\\Users\\Brux\\Downloads\\testD\\tests\\04"))
+        using (StreamReader file = new StreamReader("C:\\Users\\Brux\\Downloads\\testD\\tests\\03"))
         {
             char[] alphabeth = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".ToCharArray();
-            int[] usedLetterFirst  = new int[alphabeth.Length];
+            int[] usedLetterFirst = new int[alphabeth.Length];
             int[] usedLetterSecond = new int[alphabeth.Length];
 
             Array.Fill(usedLetterFirst, 0);
@@ -21,13 +20,7 @@ public class Program
             char[] aaa = firstWord.ToCharArray();
             char[] bbb = secondWord.ToCharArray();
 
-
-            string resultword = new String('_', firstWord.Length);
-
-
-
-            char[] resultCharArr = resultword.ToCharArray();
-
+            char[] resultCharArr = new String('_', firstWord.Length).ToCharArray();
 
             for (int i = 0; i < firstWord.Length; i++)
             {
@@ -42,13 +35,12 @@ public class Program
                 }
             }
 
-
             for (int i = 0; i < firstWord.Length; i++)
             {
                 var letterFirstWord = aaa[i];
                 var letterSecondWord = bbb[i];
 
-                var indFirst  = Array.IndexOf(alphabeth, letterFirstWord);
+                var indFirst = Array.IndexOf(alphabeth, letterFirstWord);
                 var indSecond = Array.IndexOf(alphabeth, letterSecondWord);
 
                 if (indFirst >= 0)
@@ -59,14 +51,6 @@ public class Program
                 if (indSecond >= 0)
                 {
                     usedLetterSecond[indSecond]++;
-                }
-
-                if (letterFirstWord == letterSecondWord) // если буква равна букве
-                {
-                    resultCharArr[i] = 'G';
-                    //usedLetter[indd]--;
-                    aaa[i] = ' ';
-                    bbb[i] = ' ';
                 }
             }
 
@@ -93,25 +77,7 @@ public class Program
 
                 }
             }
-            
-
-
-
-            //Console.WriteLine(String.Join("", resultCharArr));
-
-           // Console.WriteLine(String.Join(" ", aaa));
-           // Console.WriteLine(String.Join(" ", bbb));
-
-           // Console.WriteLine(String.Join(" ", alphabeth));
-            //Console.WriteLine(String.Join(" ", usedLetterFirst));
-             //   Console.WriteLine(String.Join(" ", usedLetterSecond));
-                Console.WriteLine(String.Join("", resultCharArr));
-
-                //Console.WriteLine(String.Join("+", aaa));
-
-                //Console.WriteLine(String.Join("-", bbb));
-
-
-            }
+            Console.WriteLine(String.Join("", resultCharArr));
+        }
     }
 }
